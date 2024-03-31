48eeea5815ec99e7d2ced86818366280d3411f74
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-03-31 04:22:26 +0000

    fixed index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Logan Connors
+// Created on: Mar 31 2024
 // This file contains the JS functions for index.html
+
+"use strict";
+
+// This Section calculates the TTK for Rainbow Six Siege
+function calculateTTK() {
+// This function gets the RPM, damage, and opponent health from the user
+let RPM = get
+// Then it calculates the TTK
+// Then it displays the TTK
+}
+
+// This Section calculates the Area and Volume of a Torus
+function calculateTorus() {
+// This function gets the major radius and minor radius from the user
+// Then it calculates the Area and Volume
+// Then it displays the Area and Volume
+}
